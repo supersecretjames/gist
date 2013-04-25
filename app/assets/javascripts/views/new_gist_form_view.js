2b6891521a59da182c1e5a5d55fb931fad03572b
AppAcademy Student <student@appacademy.io>
2013-04-25 14:05:26 -0700

    Add gistfile form remove button.

--- app/assets/javascripts/views/new_gist_form_view.js
+++ app/assets/javascripts/views/new_gist_form_view.js
@@ -9,11 +9,14 @@ GT.Views.NewGistFormView = Backbone.View.extend({
     var userForm = new Backbone.Form({
       model: that.model
     }).render();
+    var gistfiles = that.model.get('gist_files_attributes');
     var gistFileFormViewsArray = [];
     _.times(3, function(n) {
-      var newFormView = that.createGistFileFormView();
+      var newFormView = that.createGistFileFormView(gistfiles);
       that.addGistFileFormViews(gistFileFormViewsArray, newFormView);
     });
+
+
     var $mybutton = $('<button>Submit</button>');
     $mybutton.addClass("submit");
     $mybutton.on('click', function(e) {
@@ -25,16 +28,37 @@ GT.Views.NewGistFormView = Backbone.View.extend({
       that.model.save();
     });
     that.$el.html(userForm.el);
+
+
     _(gistFileFormViewsArray).each(function(gistFileFormView) {
       that.$el.append(gistFileFormView.render().$el);
+      var $removeButton = $('<button>Remove</button>');
+      $removeButton.on('click', function(e) {
+        var index = gistFileFormViewsArray.indexOf(gistFileFormView);
+        gistFileFormViewsArray.splice(index, 1);
+        gistfiles.remove(gistFileFormView.model)
+        $removeButton.remove();
+        gistFileFormView.render().$el.remove();
+          });
+      that.$el.append($removeButton);
     });
     that.$el.append($mybutton);
     var $addGistFileButton = $('<button>Add File</button>');
     $addGistFileButton.addClass("addFile");
     $addGistFileButton.on('click', function(e){
-      var newGistFileFormView = that.createGistFileFormView();
+      var newGistFileFormView = that.createGistFileFormView(gistfiles);
       gistFileFormViewsArray.push(newGistFileFormView);
+
+      var $removeButton = $('<button>Remove</button>');
+      $removeButton.on('click', function(e) {
+        var index = gistFileFormViewsArray.indexOf(newGistFileFormView);
+        gistFileFormViewsArray.splice(index, 1);
+        gistfiles.remove(newGistFileFormView.model)
+        $removeButton.remove();
+        newGistFileFormView.render().$el.remove();
+          });
       that.$el.append(newGistFileFormView.render().$el);
+      that.$el.append($removeButton);
       that.$el.append($mybutton);
       that.$el.append($addGistFileButton);
     });
@@ -46,9 +70,8 @@ GT.Views.NewGistFormView = Backbone.View.extend({
       array.push(newFormView);
     },
 
-  createGistFileFormView: function(){
+  createGistFileFormView: function(gistfiles){
     var that = this;
-    var gistfiles = that.model.get('gist_files_attributes');
     var gistfile = new GT.Models.GistFile();
     gistfiles.push(gistfile);
     var NewGistFileFormView = new GT.Views.NewGistFileFormView({
